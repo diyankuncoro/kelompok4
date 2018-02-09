abb1509612986b5ca9e4680946f3817c7f679aed
unknown <divahanif17@gmail.com>
2018-02-09 15:04:41 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,11 +9,52 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+//LOGIN
+Route::get('/login', function(){
+    if (Auth::check()) {
+        return redirect('/dashboard');
+    }
+    return view('login');
+})->name('login');
+Route::post('/login', 'UserController@login');
+//REGISTER
+Route::get('/register', function(){
+    if (Auth::check()) {
+        return redirect('/dashboard');
+    }
+    return view('register');
+});
+Route::post('/register', 'UserController@register');
+Route::get('/logout', function(){
+    Auth::logout();
+    return redirect('/');
+});
+Route::middleware(['auth'])->group(function () {
+   Route::get('/user', function(){
+        return view('user');
+   });
+   Route::get('/dashboard', function(){
+        return view('dashboard');
+    });
+   Route::get('/admin', function(){
+        $role = Auth::user()->role;
+        if($role != 'admin'){
+            return 'kowe ora entuk mlebu bos';
+        }
+        return view('admin.index');
+   });
+    Route::get('lomba', 'LombaController@index');
+    Route::get('lomba/add', 'LombaController@add');
+    Route::post('lomba', 'LombaController@create');
+    
+    // Edit
+    Route::get('lomba/{id}/edit', 'LombaController@edit');
+    Route::post('lomba/{id}/edit', 'LombaController@update');
+    // Delete
+    Route::get('lomba/{id}/delete', 'LombaController@delete');
+    Route::get('lomba/{id}/confirmDelete', 'LombaController@confirmDelete');
+});
+Route::get('lomba/{id}', 'LombaController@detail');
